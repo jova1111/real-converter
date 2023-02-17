a2cefb38988167374c531fd8ffcfc154495bfd09
Jovan Dragutinovic <jocandragutinovic@gmail.com>
2023-02-17 15:29:13 +0100

    Added autofocus and placeholder for converter input.

--- src/components/UnitConverter.vue
+++ src/components/UnitConverter.vue
@@ -2,8 +2,10 @@
   <div class="main-form">
     <div class="form-group">
       <input
+        ref="valueInput"
         type="number"
         class="value-input form-control"
+        placeholder="Унеси количину овде..."
         v-model="leftValue"
       />
       <select class="value-input form-control" v-model="leftUnitId">
@@ -45,7 +47,7 @@ export default {
 
   data() {
     return {
-      leftValue: 0,
+      leftValue: '',
       leftUnitId: -1,
       rightUnitId: -1,
     };
@@ -64,6 +66,7 @@ export default {
   mounted() {
     this.leftUnitId = this.units[0].id;
     this.rightUnitId = this.units[0].id;
+    this.$nextTick(() => this.$refs.valueInput.focus());
   },
 
   methods: {
@@ -73,6 +76,7 @@ export default {
       this.leftUnitId = this.rightUnitId;
       this.rightUnitId = leftUnitId;
       this.leftValue = rightValue;
+      this.$nextTick(() => this.$refs.valueInput.focus());
     },
   },
 
